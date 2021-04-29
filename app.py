5a8116e05101711f34563690afcf80cff60a7e48
Ted Schelble <ted.h.schelble@gmail.com>
2021-04-29 14:30:57 -0700

    Add pour water script and button for mvp test

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from pi import pour_water
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        pour_water.pour_water()
+        return redirect('/')
