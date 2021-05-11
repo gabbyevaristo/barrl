0326cbd3d6fa9fc5c83ae04b26026116dc210801
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-05-11 07:41:34 -0700

    added drink modal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from pi import pour_water
 from menu import drinks
+import json
 
 app = Flask(__name__)
 
@@ -12,10 +13,14 @@ def index():
     else:
         return render_template('index.html')
 
-
 @app.route('/menu', methods=["GET", "POST"])
 def menu():
     return render_template('menu.html', drinks=drinks)
+
+
+# @app.route('/getdrinklist')
+# def get_python_data():
+#     return json.dumps(drinks)
 
 
 @app.route('/cart')
