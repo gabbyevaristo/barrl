18af9af9e00d8f952aaff9ce4e4480ce51a97b9f
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-05-14 00:14:07 -0700

    fixed edit cart functionality

--- static/cart.js
+++ static/cart.js
@@ -23,47 +23,79 @@ function add_click_handlers() {
 
 // Set data for modal on cart page
 function open_cart_modal(drink, i) {
-    let cur_quantity = $("#cart-quantity-".concat(i)).text();
-    $('#modal-cart-quantity-'.concat(i)).text(cur_quantity);
+    var quantity = $("#cart-quantity-".concat(i)).text();
+    $('#modal-cart-quantity-'.concat(i)).text(quantity);
     $('#modal-cart-title-'.concat(i)).text(drink.name);
-    $('#modal-cart-price-'.concat(i)).text("$" + drink.price);
+    
+    // Multiply drink price and quantity
+    var price = parseInt(quantity) * parseFloat(drink.price);
+    $('#modal-cart-price-'.concat(i)).text("$" + price.toString());
     $('#modal-cart-'.concat(i)).modal({backdrop: 'static', keyboard: false});
 }
 
 
 // Increment and decrement quantity for modal on cart page
-$("#plus-cart-button, #minus-cart-button").click(function() {
-    let button_id = $(this).attr('id');
-    let cur_quantity_string = $("#modal-cart-quantity").text();
-    let cur_quantity_int = parseInt(cur_quantity_string);
+$(".plus-btn, .minus-btn").click(function() {
+    var i = $(this).data('index');          
+    var sign = $(this).data('identifier');
+
+    var quantity_string = $('#modal-cart-quantity-'.concat(i)).text();
+    var quantity_int = parseInt(quantity_string);
 
     // Update quantity accordingly
-    if (button_id == "plus-cart-button") {
-        if (cur_quantity_int < 4) {
-            $("#modal-cart-quantity").text((cur_quantity_int + 1).toString());
+    if (sign == "plus") {
+        if (quantity_int < 4) {
+            $('#modal-cart-quantity-'.concat(i)).text((quantity_int + 1).toString());
         } 
     } else {
-        if (cur_quantity_int >= 2) {
-            $("#modal-cart-quantity").text((cur_quantity_int - 1).toString());
+        if (quantity_int >= 2) {
+            $('#modal-cart-quantity-'.concat(i)).text((quantity_int - 1).toString());
         }
     }
 
     // Update button disabled attribute accordingly
-    if (button_id == "plus-cart-button") {
-        $("#minus-cart-button").prop('disabled', false);
-        if (cur_quantity_int > 2) {
-            $("#plus-cart-button").prop('disabled', true);
+    if (sign == "plus") {
+        $('#minus-cart-button-'.concat(i)).prop('disabled', false);
+        if (quantity_int > 2) {
+            $('#plus-cart-button-'.concat(i)).prop('disabled', true);
         }
     } else {
-        $("#plus-cart-button").prop('disabled', false);
-        if (cur_quantity_int == 2) {
-            $("#minus-cart-button").prop('disabled', true);
+        $('#plus-cart-button-'.concat(i)).prop('disabled', false);
+        if (quantity_int == 2) {
+            $('#minus-cart-button-'.concat(i)).prop('disabled', true);
         }
     }
+
+
+
+    // Get updated quantity and update price value
+    var updated_quantity = $('#modal-cart-quantity-'.concat(i)).text();
+    var drink_price = $('#cart-price-'.concat(i)).text();
+    var price = parseInt(updated_quantity) * parseFloat(drink_price);
+    $('#modal-cart-price-'.concat(i)).text("$" + price.toString());
 })
 
-// $("#update-cart-button").click(function() {
-//     let cur_quantity = $("#modal-cart-quantity").text();
-//     $('#modal-cart-1').modal('hide'); 
-//     $('#cart-quantity').text(cur_quantity);
-// })
+// Update cart with edited quantity values
+$('.update-btn').click(function() {
+    var i = $(this).data('index');
+    var quantity = $('#modal-cart-quantity-'.concat(i)).text();
+    $('#modal-cart-'.concat(i)).modal('hide'); 
+    $('#cart-quantity-'.concat(i)).text(quantity);
+})
+
+
+// Reset buttons if modal was closed out (changes should not be saved)
+$('.reset-modal').click(function() {
+    var i = $(this).data('index');
+    var quantity = $('#cart-quantity-'.concat(i)).text();
+    if (parseInt(quantity) == 1) {
+        $('#minus-cart-button-'.concat(i)).prop('disabled', true);
+        $('#plus-cart-button-'.concat(i)).prop('disabled', false);
+    } else if (parseInt(quantity) == 4) {
+        $('#minus-cart-button-'.concat(i)).prop('disabled', false);
+        $('#plus-cart-button-'.concat(i)).prop('disabled', true);
+    } else {
+        $('#minus-cart-button-'.concat(i)).prop('disabled', false);
+        $('#plus-cart-button-'.concat(i)).prop('disabled', false);
+    }
+})
