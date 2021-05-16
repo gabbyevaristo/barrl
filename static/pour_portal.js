cff8fc91edf5078703982c89c0462a5ea805fa49
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-05-16 02:18:19 -0700

    removed old code

--- static/pour_portal.js
+++ static/pour_portal.js
@@ -1,29 +1,3 @@
-// Pour button
-// $('.pour-btn').click(function() {
-//     var i = $(this).data('index');
-//     var drink_id = $(this).data('id');
-//     pour_buttons = document.getElementsByClassName('pour-btn');
-
-//     var wait_for_reload = fetch("/pour-drink", {
-//         method: "POST",
-//         body: JSON.stringify({ drink_id: drink_id }),
-//     })
-
-//     pour_buttons.forEach(function(pour_button) {
-//         pour_button.disabled = true;
-//         setTimeout(function() { 
-//             pour_button.disabled = false;
-//         }, 10000);
-//     });
-
-//     setTimeout(function(){
-//         window.location.reload();
-//     }, 10000)
-
-//     $('#pour-item-'.concat(i)).remove();
-// })
-
-
 window.onload = set_buttons;
 
 function set_buttons() {
@@ -39,7 +13,6 @@ function set_buttons() {
                 body: JSON.stringify({ drink_id: drink_id }),
             })
 
-            console.log(pour_buttons.length);
             if (pour_buttons.length > 1) {
                 pour_buttons.forEach(function(pour_button) {
                     pour_button.disabled = true;
