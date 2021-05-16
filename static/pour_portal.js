1c410b5a6dd46fad844f41cfb3c21f8f20aec07b
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-05-15 21:12:58 -0700

    added timeout on buttons on pour portal page

--- static/pour_portal.js
+++ static/pour_portal.js
@@ -3,13 +3,24 @@ $('.pour-btn').click(function() {
     var i = $(this).data('index');
     var drink_id = $(this).data('id');
     
-    fetch("/pour-drink", {
-        method: "POST",
-        body: JSON.stringify({ drink_id: drink_id }),
-    })
-    .then((_res) => {
-        window.location.href = "/pour-portal";
+    async () => {
+        await fetch("/pour-drink", {
+            method: "POST",
+            body: JSON.stringify({ drink_id: drink_id }),
+        })
+        .then((_res) => {
+            window.location.href = "/pour-portal";
+        })
+    }
+
+    pour_buttons = document.getElementsByClassName('pour-btn');
+    pour_buttons.forEach(function(pour_button) {
+        pour_button.disabled = true;
+        setTimeout(function() { 
+            pour_button.disabled = false;
+        }, 10000);
     });
 
     $('#pour-item-'.concat(i)).remove();
 })
+
