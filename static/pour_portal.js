1c6d70fba4ced975fa9bc476d38cd0667e20835b
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-05-16 02:09:28 -0700

    fixed disabling of buttons on pour page

--- static/pour_portal.js
+++ static/pour_portal.js
@@ -1,24 +1,63 @@
 // Pour button
-$('.pour-btn').click(function() {
-    var i = $(this).data('index');
-    var drink_id = $(this).data('id');
+// $('.pour-btn').click(function() {
+//     var i = $(this).data('index');
+//     var drink_id = $(this).data('id');
+//     pour_buttons = document.getElementsByClassName('pour-btn');
+
+//     var wait_for_reload = fetch("/pour-drink", {
+//         method: "POST",
+//         body: JSON.stringify({ drink_id: drink_id }),
+//     })
+
+//     pour_buttons.forEach(function(pour_button) {
+//         pour_button.disabled = true;
+//         setTimeout(function() { 
+//             pour_button.disabled = false;
+//         }, 10000);
+//     });
+
+//     setTimeout(function(){
+//         window.location.reload();
+//     }, 10000)
+
+//     $('#pour-item-'.concat(i)).remove();
+// })
+
+
+window.onload = set_buttons;
+
+function set_buttons() {
     pour_buttons = document.getElementsByClassName('pour-btn');
 
-    fetch("/pour-drink", {
-        method: "POST",
-        body: JSON.stringify({ drink_id: drink_id }),
-    })
-    .then((_res) => {
-            window.location.href = "/pour-portal";
-    })
-
     pour_buttons.forEach(function(pour_button) {
-        pour_button.disabled = true;
-        setTimeout(function() { 
-            pour_button.disabled = false;
-        }, 4000);
+        pour_button.addEventListener('click', function(e) {
+            var i = $(e.target).data('index');
+            var drink_id = $(e.target).data('id');
+            
+            fetch("/pour-drink", {
+                method: "POST",
+                body: JSON.stringify({ drink_id: drink_id }),
+            })
+
+            console.log(pour_buttons.length);
+            if (pour_buttons.length > 1) {
+                pour_buttons.forEach(function(pour_button) {
+                    pour_button.disabled = true;
+                    setTimeout(function() { 
+                        pour_button.disabled = false;
+                    }, 5000);
+                });
+                    
+                setTimeout(function(){
+                    window.location.reload();
+                }, 5000);
+            } else {
+                setTimeout(function(){
+                    window.location.reload();
+                }, 1000);
+            }
+    
+            $('#pour-item-'.concat(i)).remove();
+        });
     });
-
-    $('#pour-item-'.concat(i)).remove();
-})
-
+}
