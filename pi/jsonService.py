e252a5e1b9c3b0587e79ae4d057e9a4b42e82023
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-06-04 17:58:29 -0700

    cleaned up json service file

--- pi/jsonService.py
+++ pi/jsonService.py
@@ -3,11 +3,11 @@ import json
 
 def loadJson(filePath):
     ret = None
-    with open(filePath, "r") as file:
+    with open(filePath, 'r') as file:
         ret = json.load(file)
     return ret
 
 
 def saveJson(obj, filePath):
-    with open(filePath, "w") as file:
+    with open(filePath, 'w') as file:
         json.dump(obj, file, indent=4, sort_keys=True)
